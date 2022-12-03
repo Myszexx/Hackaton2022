99624ae7f5209dfee4bbdbbad07c220c014c950d
Kris434 <77887640+Kris434@users.noreply.github.com>
2022-12-03 11:25:53 +0100

    Update tasksPost.php

--- api/tasksPost.php
+++ api/tasksPost.php
@@ -8,6 +8,11 @@ if (!empty($_POST))
     $sql = "INSERT INTO tasks (`user_id`, `time`, `task_type`, `comment`, `title`, `priority`, `alerts`, `color`) VALUES (" . $_SESSION['user_id'] . "," . $_POST['time'] . "," . $_POST['type'] . "," . $_POST['comment'] . "," . $_POST['title'] . "," . $_POST['priority'] . "," . $_POST['alerts'] . "," . $_POST['colors'] . ")";
 
     $res = $conn -> query($sql);
+
+    if(!$res)
+    {
+        echo "Nie można włożyć danych do bazy";
+    }
 }
 else
 {
