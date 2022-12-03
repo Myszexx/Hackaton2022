fdf1dae2407e739865147e7d230a58c7e54c982a
Kris434 <77887640+Kris434@users.noreply.github.com>
2022-12-03 02:59:00 +0100

    Harmonogram api gotowe

--- api/harmonogram.php
+++ api/harmonogram.php
@@ -1,9 +1,59 @@
 <?php
-if(isset($_POST['length']) && isset($_POST['startday']))
+session_start();
+header('Content-Type: application/json; charset: UTF-8');
+
+$json = [];
+$time = [];
+$task = [];
+
+require_once "../accounts/connection.php";
+
+if(isset($_POST['length']) && isset($_POST['startday']) && isset($_SESSION['user_id']))
 {
     $length = $_POST['length'];
     $startday = $_POST['startday'];
 
-    $sql = "SELECT id, start_date, end_date, type FROM free_time WHERE user_id = "+$user_id_z_sesji+" AND start_date BEETWEEN sysdate AND SYSDATE+"+$_POST['length'];
-    $sql = "SELECT taks_id, task_type, time, comment, title, priority,alerts,color FROM tasks WHERE user_id = "+$user_id_z_sesji+" AND alerts BEETWEEN sysdate AND SYSDATE+"+$_POST['length'];
+    $sql1 = "SELECT `id`, `start_date`, `end_date`, `type` FROM free_time WHERE user_id = " . $_SESSION['user_id'] . " AND start_date BETWEEN sysdate() AND SYSDATE()+" . $length;
+    $sql2 = "SELECT `task_id`, `task_type`, `time`, `comment`, `title`, `priority`, `alerts`, `color` FROM tasks WHERE user_id = " . $_SESSION['user_id'] . " AND alerts BETWEEN sysdate() AND SYSDATE()+" . $length;
+
+    $res1 = $conn -> query($sql1);
+    $res2 = $conn -> query($sql2);
+
+    $res1_num_rows = $res1 -> num_rows;
+    $res2_num_rows = $res2 -> num_rows;
+
+    $res1 = $res1 -> fetch_assoc();
+    $res2 = $res2 -> fetch_assoc();
+
+    for($i = 0; $i < $res1_num_rows; $i++)
+    {
+        $time[$i] = [
+            "id" => $res1['id'],
+            "start_date" => $res1['start_date'],
+            "end_date" => $res1['end_date'],
+            "type" => $res1['type']
+        ];
+    }
+
+    for($i = 0; $i < $res2_num_rows; $i++)
+    {
+        $task[$i] = [
+            "task_id" => $res2['task_id'],
+            "time" => $res2['time'],
+            "type" => $res2['type'],
+            "comment" => $res2['comment'],
+            "title" => $res2['title'],
+            "priority" => $res2['priority'],
+            "alerts" => $res2['alerts'],
+            "colors" => $res2['colors']
+        ];
+    }
+
+
+    $json = [
+        "tasks" => $task,
+        "time" => $time
+    ];
+
+    echo json_encode($json);
 }
